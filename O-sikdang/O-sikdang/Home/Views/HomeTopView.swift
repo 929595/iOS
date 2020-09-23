7ce20349ae3e216ef1c1143fe66f378b959b4dbc
Cory Kim <corykim0829@gmail.com>
2020-09-23 18:11:51 +0900

    feat: HomeTopView layout
    #2

---
+++ O-sikdang/O-sikdang/Home/Views/HomeTopView.swift
@@ -0,0 +1,18 @@
+import UIKit
+
+final class HomeTopView: UIView {
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configure()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        configure()
+    }
+    
+    private func configure() {
+        
+    }
+}
--- O-sikdang/O-sikdang/ViewController.swift
+++ O-sikdang/O-sikdang/ViewController.swift
@@ -1,9 +1,37 @@
 import UIKit
+import SnapKit
 
 final class HomeViewController: UIViewController {
 
+    private var homeTopView: HomeTopView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configure()
+    }
+}
+
+extension HomeViewController {
+    private func configure() {
+        configureViews()
+        configureSubviews()
+        configureViewLayouts()
+    }
+    
+    private func configureViewLayouts() {
+        homeTopView.snp.makeConstraints { (make) -> Void in
+            make.leading.equalTo(view.snp.leading)
+            make.trailing.equalTo(view.snp.trailing)
+            make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
+            make.height.equalTo(100)
+        }
+    }
+    
+    private func configureSubviews() {
+        view.addSubview(homeTopView)
+    }
+    
+    private func configureViews() {
+        homeTopView = HomeTopView.loadFromNib()
     }
 }
