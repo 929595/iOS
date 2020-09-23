b0912ae265312f5e4ad492a07b1cb246b9e7fa37
Cory Kim <corykim0829@gmail.com>
2020-09-23 18:28:49 +0900

    feat: HomeTodayChatBubble layout
    #2

--- O-sikdang/O-sikdang/ViewController.swift
+++ O-sikdang/O-sikdang/ViewController.swift
@@ -4,12 +4,15 @@ import SnapKit
 final class HomeViewController: UIViewController {
 
     private var homeTopView: HomeTopView!
+    private var todayChatBubbleView: HomeTodayChatBubbleView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         configure()
     }
 }
+
+// MARK: - Configuration
 
 extension HomeViewController {
     private func configure() {
@@ -25,13 +28,21 @@ extension HomeViewController {
             make.top.equalTo(view.safeAreaLayoutGuide.snp.top)
             make.height.equalTo(100)
         }
+        todayChatBubbleView.snp.makeConstraints { (make) -> Void in
+            make.leading.equalTo(view.snp.leading)
+            make.trailing.equalTo(view.snp.trailing)
+            make.top.equalTo(homeTopView.snp.bottom)
+            make.height.equalTo(120)
+        }
     }
     
     private func configureSubviews() {
         view.addSubview(homeTopView)
+        view.addSubview(todayChatBubbleView)
     }
     
     private func configureViews() {
         homeTopView = HomeTopView.loadFromNib()
+        todayChatBubbleView = HomeTodayChatBubbleView.loadFromNib()
     }
 }
